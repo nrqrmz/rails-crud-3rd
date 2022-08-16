c18a12a027443f9ad907c441e69998b26e49e4ce
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 19:37:33 -0500

    Add delete restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
   post "restaurants", to: "restaurants#create"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit", as
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
   patch "restaurants/:id", to: "restaurants#update"
   delete "restaurants/:id", to: "restaurants#destroy"
 end
