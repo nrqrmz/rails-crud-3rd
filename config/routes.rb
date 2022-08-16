fc31725c809aecd74af73c84ce3a1227259c49a8
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 19:32:46 -0500

    Add update restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get "restaurants/new", to: "restaurants#new"
   post "restaurants", to: "restaurants#create"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit"
+  get "restaurants/:id/edit", to: "restaurants#edit", as
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
