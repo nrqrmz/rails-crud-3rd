c83da38f0eecd043cf5c9a1484a58250f88ab503
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 19:22:13 -0500

    Add create restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
   get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
 end
